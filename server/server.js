5165f91d0671a5c514aceb075b64e728636f2e4a
Ishan Farooq <ishaanfarooq001@gmail.com>
2026-02-28 07:55:02 +0530

    added the correct mongo uri]

--- server/server.js
+++ server/server.js
@@ -6,7 +6,9 @@ const cors = require("cors");
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
